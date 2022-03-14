f027805e7ad3c3c7977ea411b189a34472c7dbe7
Proniaiev Maksym <maxse828@gmail.com>
2022-03-14 23:10:04 +0200

    add order check

---
+++ go/isOrderCorrect/checkOrder.go
@@ -0,0 +1,23 @@
+package IsOrderCorrect
+
+func isValid(s string) bool {
+	st := []rune{}
+	bracketsMap := map[rune]rune{
+		')': '(',
+		'}': '{',
+		']': '[',
+	}
+	for _, v := range s {
+		if len(st) == 0 {
+			st = append(st, v)
+			continue
+		}
+		if bracketsMap[v] == st[len(st)-1] {
+			st = st[:len(st)-1]
+		} else {
+			st = append(st, v)
+		}
+	}
+
+	return len(st) == 0
+}
--- go/main.go
+++ go/main.go
@@ -1,9 +1,18 @@
 package main
 
 import (
-	rev "radioemptinessgoleetcode/reverseString"
+	"fmt"
+	ua "radioemptinessgoleetcode/IsOrderCorrect"
 )
 
 func main() {
-	rev.Reverse()
+	var input = "()[]{}"
+	res := ua.IsValid(input)
+
+	fmt.Println(res)
+
+	var inp2 = "(]"
+	res2 := ua.IsValid(inp2)
+
+	fmt.Println(res2)
 }
